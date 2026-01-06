eac5bd1779ac641523e1dcd89041d884ee85f106
PrinceOfPickles <x3Nu5QX@protonmail.com>
2026-01-06 19:30:22 +0200

    added User model with 2FA support

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import EnergyDrink, Review
+from .models import EnergyDrink, Review, DrinkType, DrinkContainer
 
 # Register your models here.
 admin.site.register(EnergyDrink)
 admin.site.register(Review)
+admin.site.register(DrinkType)
+admin.site.register(DrinkContainer)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    otp_secret = models.CharField(max_length=32, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
